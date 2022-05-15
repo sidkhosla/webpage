97e89a32e6e8bd48691090b87fe83c980f8f636c
sidkhosla <sidkhosla2@gmail.com>
2022-05-15 21:01:12 +0100

    file organiser project

--- Module_1/fileOrganiser/createStructure.js
+++ Module_1/fileOrganiser/createStructure.js
@@ -3,17 +3,25 @@ let fs = require("fs");
 let modules = ["Javascript", "Browser", "React", "Backend", "Git"];
 
 // path build
-let webDevFolderPath = path.join(process.cwd(), "webdev");
+let webDevDirPath = path.join(process.cwd(), "webdev");
 
 // checking if the path exsist
 
-if (fs.existsSync(webDevFolderPath)){
+if (fs.existsSync(webDevDirPath)){
     //then i dont have to make it
-    console.log(webDevFolderPath);
+    console.log(webDevDirPath, " already exsists!");
+} else {
+    fs.mkdirSync(webDevDirPath);
 }
 
 // create all the modules inside your web dev directory
 
 for(let i = 0; i < modules.length; i++){
-    let 
+    let modulePath = path.join(webDevDirPath, modules[i]);
+
+    if(fs.existsSync(modulePath)){
+        console.log(modules[i], " already exsists");
+    }else {
+        fs.mkdirSync(modulePath)
+    }
 }
